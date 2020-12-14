3448318c402609e032221081c7ca4bbab1ddbcc5
xiazeyin <xiazeyin@peogoo.com>
2020-12-14 22:42:58 +0800

    modify BlockFee struct

--- model/fee.go
+++ model/fee.go
@@ -1,7 +1,7 @@
 package model
 
 type BlockFee struct {
-	FastestFee	uint64	`json:"fastestFee"`
-	HalfHourFee	uint64	`json:"halfHourFee"`
-	HourFee		uint64	`json:"hourFee"`
+	FastestFee	int	`json:"fastestFee"`
+	HalfHourFee	int	`json:"halfHourFee"`
+	HourFee		int	`json:"hourFee"`
 }
