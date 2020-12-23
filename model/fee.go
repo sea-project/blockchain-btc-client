fa0564810234a05f1c0fb110157159347dfadac0
xiazeyin <xiazeyin@peogoo.com>
2020-12-23 17:36:52 +0800

    手续费对象添加string方法

--- model/fee.go
+++ model/fee.go
@@ -1,7 +1,14 @@
 package model
 
+import "encoding/json"
+
 type BlockFee struct {
 	FastestFee	int	`json:"fastestFee"`
 	HalfHourFee	int	`json:"halfHourFee"`
 	HourFee		int	`json:"hourFee"`
+}
+
+func (b *BlockFee) String() string {
+	str, _ := json.Marshal(b)
+	return string(str)
 }
